a831f6b026ac7606cb83547633768b8afd1539bd
Elliott Burr <elliott.burr@openfin.co>
2023-04-05 19:22:06 -0400

    update script

--- common/getBranchName.js
+++ common/getBranchName.js
@@ -1,10 +1,15 @@
 console.log(process.argv)
 
-const [LABELS, BRANCH_NAME] = process.argv.slice(2);
+const [LABELS, BRANCH_NAME, ...args] = process.argv.slice(2);
 
 LABELS.filter((name) => {
     name.startsWith(BRANCH_NAME)
 })
+
+if (args.length > 0) {
+    console.log('This script only requires 2 arguments.');
+    process.exit(1);
+};
 
 if(LABELS.length > 1){
     console.log(`There can only be 1 branch that begins with ${BRANCH_NAME}`);
