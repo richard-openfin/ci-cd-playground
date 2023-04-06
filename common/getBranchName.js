d9add755ca205b09f1e832f595b5a6925d0a556e
Elliott Burr <elliott.burr@openfin.co>
2023-04-05 22:05:59 -0400

    f

--- common/getBranchName.js
+++ common/getBranchName.js
@@ -26,6 +26,6 @@ if(filteredArray.length > 1){
     process.exit(1);
 } else {
     console.log(filteredArray[0])
-    // process.stdout.write(`::set-output name=branch::${filteredArray[0]}`)
-    process.stdout.write(`branch=${filteredArray[0]} >> $GITHUB_OUTPUT`)
+    process.stdout.write(`::set-output name=branch::${filteredArray[0]}`)
+    // process.stdout.write(`branch=${filteredArray[0]} >> $GITHUB_OUTPUT`)
 }
